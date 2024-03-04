d133fa1342b9b3ba1cd5411f39ad83f205fc6c41
wylu <wylu@zkjg.com>
2024-03-04 08:07:49 +0800

    feat: add Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,11 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} relative bg-gray-50 text-green-950 pt-28 sm:pt-36`}>
+      <body
+        className={`${inter.className} relative bg-gray-50 text-green-950 pt-28 sm:pt-36`}
+      >
         <div className="absolute -z-10 top-[-6rem] right-[11rem] bg-[#fbe2e3] h-[31.25rem] w-[31.25rem] rounded-full blur-[11rem] sm:w-[68.75rem]"></div>
         <div className="absolute -z-10 top-[-1rem] left-[-35rem] bg-[#dbd7fb] h-[31.25rem] w-[50rem] rounded-full blur-[11rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/About.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='mb-5'>About</div>
+  )
+}
