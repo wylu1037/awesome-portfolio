fb8256f77be055bc0fbc3aeed390935925b6aaf0
wylu <wylu@zkjg.com>
2024-03-05 23:31:38 +0800

    fix: yarn run build failed

--- components/Project.tsx
+++ components/Project.tsx
@@ -14,7 +14,7 @@ export default function Project({
   tags,
   imageUrl,
 }: ProjectProps) {
-  const ref = useRef<HTMLElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["0 1", "1.33 1"],
