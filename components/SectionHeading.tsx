6a3c49de69c9ceff4a6d18a7164c33fed1a31f27
wylu <wylu@zkjg.com>
2024-03-06 14:05:46 +0800

    chore: adjust delay value

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -8,7 +8,7 @@ export default function SectionHeading({ children }: SectionHeadingProps) {
     <motion.h2
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.2 }}
+      transition={{ delay: 0.175 }}
       className="text-center text-3xl font-medium capitalize mb-8"
     >
       {children}
