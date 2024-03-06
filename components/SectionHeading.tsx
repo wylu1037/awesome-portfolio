d210438bc551675c4d036997784d717c3e2c794f
wylu <wylu@zkjg.com>
2024-03-06 13:48:51 +0800

    feat: add motion animation to Heading component

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -1,11 +1,17 @@
+import { motion } from "framer-motion";
 import React from "react";
 
 type SectionHeadingProps = Readonly<{ children: React.ReactNode }>;
 
 export default function SectionHeading({ children }: SectionHeadingProps) {
   return (
-    <h2 className="text-center text-3xl font-medium capitalize mb-8">
+    <motion.h2
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+      className="text-center text-3xl font-medium capitalize mb-8"
+    >
       {children}
-    </h2>
+    </motion.h2>
   );
 }
