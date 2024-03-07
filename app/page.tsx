2ed89de533b1a9aca815c79ec6ccaba131670c6c
wylu1037 <wylu1037@gmail.com>
2024-03-07 20:21:04 +0800

    feat: add Experience component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Experience />
     </div>
   );
 }
