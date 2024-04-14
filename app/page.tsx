5eac1fd9b1801680782cbbe4ad372d7251e222d9
wylu1037 <wylu1037@gmail.com>
2024-04-14 15:26:56 +0800

    fix: build failed

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-import About from "@/components/portfolio/About";
-import Contact from "@/components/portfolio/Contact";
-import Experience from "@/components/portfolio/Experience";
-import Footer from "@/components/portfolio/Footer";
-import Header from "@/components/portfolio/Header";
-import Intro from "@/components/portfolio/Intro";
-import Projects from "@/components/portfolio/Projects";
-import Skills from "@/components/portfolio/Skills";
-import ToggleTheme from "@/components/portfolio/ToggleTheme";
-import SectionDivider from "@/components/portfolio/SectionDivider";
-import ActiveSectionContextProvider, {
-  ActiveSectionContext,
-} from "@/context/active-section-context";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const validateString = (
+    value: unknown,
+    maxLength: number
+): value is string => {
+  if (!value || typeof value !== "string" || value.length > maxLength) {
+    return false;
+  }
+
+  return true;
+};
