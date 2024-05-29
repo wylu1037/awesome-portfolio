1bf20af6ea37c4392dfbe952f374109f46d4e49c
wylu1037 <wylu1037@gmail.com>
2024-05-29 12:34:33 +0800

    chore: add button in index

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <Button variant="outline" asChild>
-        <Link href="portfolio">View Portfolio</Link>
-      </Button>
-    </div>
-  );
+    return (
+        <div className="flex justify-center items-center h-screen space-x-2">
+            <Button variant="outline" asChild>
+                <Link href="portfolio">View Portfolio</Link>
+            </Button>
+            <Button variant="ghost">
+                <Link href="example">Explore Example</Link>
+            </Button>
+        </div>
+    );
 }
